7469493656ce1d9914aa1b82adbefdb8d5d7044e
KOVACS Nicolas <perso.nkovacs@gmail.com>
2016-06-03 10:14:17 +0200

    fix rails_admin_theme 1.0 integ

--- app/assets/javascripts/rails_admin/custom/ui.js
+++ app/assets/javascripts/rails_admin/custom/ui.js
@@ -25,38 +25,8 @@
 //= require plugins/url.min.js
 //= require plugins/video.min.js
 //= require languages/fr.js
-//= require icheck
 
 $(document).on('ready pjax:success', function() {
-  $('input').iCheck({
-    checkboxClass: 'icheckbox_flat-grey selectable',
-    radioClass: 'iradio_flat-grey'
-  });
-
-  var checkBox = $('.table-striped > tbody > tr > td:first-child input[type="checkbox"]');
-  var togglerCheck = $('th.shrink input[type="checkbox"]');
-
-  checkBox.on('ifChecked', function(e) {
-    $(this).parent().parent().parent().addClass('row-highlight');
-  });
-  checkBox.on('ifUnchecked', function(e) {
-    $(this).parent().parent().parent().removeClass('row-highlight');
-  });
-
-  togglerCheck.on('ifChecked', function(e) {
-    checkBox.iCheck('check');
-    handleHighlight();
-  });
-  togglerCheck.on('ifUnchecked', function(e) {
-    checkBox.iCheck('uncheck');
-    $('.table-striped tbody tr').removeClass('row-highlight');
-  });
-  function handleHighlight() {
-    $('.table-striped tbody td').has('div.checked').each(function(index, item) {
-      $(item).parent().addClass('row-highlight');
-    });
-  }
-
   handleActiveBase();
   function handleActiveBase() {
     $('.sub-menu').each(function () {
