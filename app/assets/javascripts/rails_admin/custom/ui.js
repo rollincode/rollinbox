da08e49724c19d18dbf11f835e4d91b264d1d9c7
Matthieu <matthv@gmail.com>
2016-10-03 13:45:00 +0200

    Update ui.js

--- app/assets/javascripts/rails_admin/custom/ui.js
+++ app/assets/javascripts/rails_admin/custom/ui.js
@@ -38,7 +38,9 @@ $(document).on('ready pjax:success', function(e) {
         });
     }
     // home dashboard dark theme
-    if(new RegExp('admin/'+'$').test(e.currentTarget.URL) || new RegExp('admin'+'$').test(e.currentTarget.URL)){
+    if((new RegExp('admin/$').test(e.currentTarget.URL)
+        || new RegExp('admin$').test(e.currentTarget.URL))
+        && !new RegExp('admin/admin').test(e.currentTarget.URL)){
         $('.page-header, .content').addClass('dashboard');
     }
     else {
