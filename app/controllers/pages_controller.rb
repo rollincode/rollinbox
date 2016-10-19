21c44e81db9a0d9a6f3e4c3b333b7314cd6294aa
Kovacs Nicolas <perso.nkovacs@gmail.com>
2016-10-19 10:49:25 +0200

    Page controller features

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,13 @@
 class PagesController < ApplicationController
   before_filter :find_page
 
-  def single
-    @page = Page.find params[:slug]
+  def show
+    @page = Page.friendly.find params[:id]
   end
 
   def find_page
-    @page = Page.find params[:id]
-    if request.path != post_path(@page)
+    @page = Page.friendly.find params[:id]
+    if request.path != page_path(@page)
       return redirect_to @page, status: :moved_permanently
     end
   end
