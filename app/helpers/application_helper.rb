79be99328e80712698053ffa39776845d137b66c
Matthieu <matthv@gmail.com>
2017-01-13 00:28:34 +0100

    add new helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,18 @@ module ApplicationHelper
   def meta_description(meta_description)
     content_for(:meta_description) { meta_description }
   end
+  
+  def og_title(og_title)
+    content_for(:og_title) { og_title }
+  end
+  
+  def og_description(og_description)
+    content_for(:og_description) { og_description }
+  end
+  
+  def og_image(og_image)
+    content_for(:og_image) { og_image }
+  end
 
   def bootstrap_class_for(flash_type)
     case flash_type
